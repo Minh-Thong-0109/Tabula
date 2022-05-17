11718f1477e35e68505c7d82c3dae1adf04bc120
Minh Thông <minhthong.nguyen.38@gmail.com>
2022-05-17 21:06:46 +0700

    updated

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,33 @@ function getEle(id) {
   return document.getElementById(id);
 }
 
+// CSS Navbar
+const header = document.querySelector("header");
+function onScrollHandler(event) {
+    if (window.scrollY > 0) {
+        header.style.position = "fixed";
+        header.style.width = "100%";
+        header.style.zIndex = "100";
+        header.style.top = "0";
+        header.style.backgroundColor = "rgba(255,255,255,1)";
+        getEle("navbar-collapse").style.margin ="0 auto"; 
+        getEle("header__menu").style.padding = "0.5rem 0";
+        getEle("logo").style.display = "none";
+        header.style.boxShadow = "0 0 30px rgba(0,0,0,0.8)";
+    } else {
+        header.style.position = null;
+        header.style.width = null;
+        header.style.zIndex = null;
+        header.style.top = null;
+        header.style.backgroundColor = "rgba(255,255,255,0)";
+        getEle("header__menu").style.padding = null;
+        getEle("logo").style.display = "block";
+        header.style.boxShadow = null;
+    }
+}
+
+window.addEventListener("scroll", onScrollHandler);
+
 services = new Services();
 getList();
 
